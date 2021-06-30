99c68c8c3eff9af4b54659f54f101b9bb44eb41d
Deekshith76 <deekshith_b180791ce@nitc.ac.in>
2021-06-30 10:24:16 +0530

    Fixed the default dict problem

--- canSum-memoization.py
+++ canSum-memoization.py
@@ -24,28 +24,30 @@ def canSum(targetSum, numbers):
 #print(canSum(8, [2,3,5])) # => Time Complexity: O(n**m) => Space Complexity: O(m)
 
 '''Memoizing it'''
-'''memo will be initialised only once. So, if you are not passing memo while calling then the previous memo will be used which is not advisable in this case'''
-def canSum2(targetSum, numbers):
-    memo = dict() 
-    def helper(targetSum, numbers):
-        if targetSum in memo:
-            return memo[targetSum]
-        if targetSum == 0:
-            return True
-        if targetSum < 0:
-            return False
-        for num in numbers:
-            remainder = targetSum - num
-            if(canSum2(remainder, numbers) == True):
-                memo[targetSum] = True
-                return True
-        memo[targetSum] = False
+'''memo will be initialised only once. So, if you are not passing memo while calling then the previous memo will be used which is not advisable in this case. In other words the value of default argument is evaluated only once, when the function is declared'''
+
+'''A common way is to create a new mutable object by default everytime a function is called is to use an immutable placeholder value like None. The function looks for that placeholder value and when found, replaces it with a brand new instance of mutable type'''
+
+
+def canSum2(targetSum, numbers, memo=None):
+    if memo is None:
+        memo = {}
+    if targetSum in memo:
+        return memo[targetSum]
+    if targetSum == 0:
+        return True
+    if targetSum < 0:
         return False
-    return helper(targetSum, numbers)
-
-
+    for num in numbers:
+        remainder = targetSum - num
+        if(canSum2(remainder, numbers, memo)==True):
+            memo[targetSum] = True
+            return True
+    memo[targetSum] = False
+    return False
+    
+print(canSum2(777, [2,4,6]))
 print(canSum2(7, [2,3]))
 print(canSum2(1, [5,3,4,7]))
 print(canSum2(7, [2,4]))
-print(canSum2(8, [2,3,5]))   
-print(canSum2(333, [2,4,6]))
+print(canSum2(8, [2,3,5]))

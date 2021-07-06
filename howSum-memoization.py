3fe20c04ea1ca49d07ab5d91372415cf9109d190
Deekshith76 <deekshith_b180791ce@nitc.ac.in>
2021-07-06 16:02:55 +0530

    Added tabulation method

--- howSum-memoization.py
+++ howSum-memoization.py
@@ -49,7 +49,28 @@ print(howSum2(7, [2,3]))
 print(howSum2(7, [5,3,4,7])) 
 print(howSum2(7, [2,4]))
 print(howSum2(8, [2,3,5]))
-print(howSum2(300, [14,7,10,2])) 
+#print(howSum2(300, [14,7,10,2])) 
 
 """Time complexity: O(n * m^2)
     Space complexity: O(m * m) = O(m^2)"""
+
+# Tabulation method
+
+def howSumt(targetSum, numbers):
+    table = [None] * (targetSum + 1)
+    table[0] = []
+    for i in range(targetSum + 1):
+        if table[i] is not None:
+            numbers = [num for num in numbers if i+num <=targetSum]
+            for num in numbers:
+                table[i+num] = table[i] + [num]
+    return table[targetSum]
+    
+print(howSumt(7, [5,3,4]))
+print(howSum2(7, [2,4]))
+print(howSum2(300, [14,7,10,2]))
+
+'''
+Time Complexity: O(m^2 * n)
+Space Complexity: O(m^2)
+'''
